538a2649f355e03bee4513488af4ed1d7105411f
camera-2018 <2907618001@qq.com>
2023-01-21 15:35:20 +0800

    feat: sign aside

--- frontend/components/Aside/index.vue
+++ frontend/components/Aside/index.vue
@@ -1,11 +1,25 @@
 <template lang="">
-  <div>
+  <div class="index-aside">
+    <AsideSign />
     <h1 class="text-center">
       这里是广告
     </h1>
+    2333
   </div>
 </template>
 
-<style lang="">
+<style scoped>
+.sidebar-block {
+  background-color: #fff;
+  box-shadow: 0 1px 2px 0 rgb(0 0 0 / 5%);
+  border-radius: 2px;
+  margin-bottom: 1.3rem;
+  font-size: 1.16rem;
+  line-height: 1.29;
+  color: #333;
+}
 
+.index-aside {
+  width: 20rem;
+}
 </style>
---
+++ frontend/components/Aside/sign.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+</script>
+
+<template>
+  <div display="block" class="">
+    <div class="bg-light-50 p-5 py-8">
+      <div class="flex flex-row gap-8">
+        <div class="flex flex-col gap-2">
+          <span class="text-black font-bold text-3xl">晚上好！</span>
+          <div class="text-[#8a919f] text-xl">
+            点亮在社区的每一天
+          </div>
+        </div>
+        <button class="bg-[#f4f9ff] border-[#8dbfff] hover:bg-[#e8f2ff] border-solid border-1 rounded-2">
+          <span class="text-2xl text-[#1e80ff] p-3 px-7">去签到</span>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
