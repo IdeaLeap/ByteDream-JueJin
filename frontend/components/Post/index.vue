0daa5e40ea35429490357a941f8bce68d5f8aba6
Plumbiu <99574369+Plumbiu@users.noreply.github.com>
2023-01-22 16:26:55 +0800

    feat: 增加骨架屏和下拉框

--- frontend/components/Post/index.vue
+++ frontend/components/Post/index.vue
@@ -1,6 +1,13 @@
 <script setup lang="ts">
 import type { IPanel } from './IPanel'
 let pagenum = 0
+const isLoading = useState('isLoading', () => false)
+const artlistNav = useState('artlistNav', () => 'recommend')
+const hotRange = useState('hotRange', () => 'all')
+const hotRangeHandler = () => {
+  // eslint-disable-next-line no-console
+  console.log(hotRange.value)
+}
 const initialItem = await useFetchPostData()
 const artlistData = useState('artlist', () => [...initialItem] as IPanel[])
 const addArtListItem = () => {
@@ -16,6 +23,7 @@ const addArtListItem = () => {
   }
 }
 const artModeHandler = (mode: string) => {
+  artlistNav.value = mode
   const allEl = document.querySelectorAll('.navlist') as unknown as HTMLElement[]
   allEl.forEach((item) => {
     item.style.color = '#6b7280'
@@ -39,24 +47,38 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <div class="all-text-black">
-    <div class="flex all-cursor-pointer all-px-7 all-my-4 border-b-1 all-text-lg all-text-gray-500">
+  <div class="all-text-black pb-5 px-5 box-border">
+    <div class="flex all-cursor-pointer all-px-7 all-my-4 border-b-1 all-text-gray-500" style="font-size: 1.35rem;">
       <a id="recommend" class="text-blue navlist border-r-2" @click="artModeHandler('recommend')">推荐</a>
       <a id="latest" class="navlist border-r-2" @click="artModeHandler('latest')">最新</a>
       <a id="hot" class="navlist" @click="artModeHandler('hot')">热榜</a>
+      <select
+        v-if="artlistNav === 'hot'" v-model="hotRange" class="text-2 px-2 border-1"
+        @change="hotRangeHandler"
+      >
+        <option class="" :value="24 * 60 * 60 * 1000 * 3">
+          3天内
+        </option>
+        <option class="" :value="24 * 60 * 60 * 1000 * 7">
+          7天内
+        </option>
+        <option class="" :value="24 * 60 * 60 * 1000 * 30">
+          30天内
+        </option>
+        <option value="all">
+          全部
+        </option>
+      </select>
     </div>
-    <ul>
+    <ul v-if="!isLoading || initialItem">
       <PostItem
-        v-for="items in artlistData"
-        :key="items.uname"
-        :uname="items.uname"
-        :duration="items.duration"
-        :title="items.title"
-        :desc="items.desc"
-        :tags="items.tags"
-        :topic-heat="items.topicHeat"
+        v-for="items in artlistData" :key="items.uname" :uname="items.uname" :duration="items.duration"
+        :title="items.title" :desc="items.desc" :tags="items.tags" :topic-heat="items.topicHeat"
       />
     </ul>
+    <div v-else>
+      <PostSkeleton />
+    </div>
   </div>
 </template>
 
@@ -64,8 +86,5 @@ onUnmounted(() => {
 a {
   transition: all .15s ease;
   color: '#6b7280';
-}
-a:hover {
-  color: deepskyblue!important;;
 }
 </style>
