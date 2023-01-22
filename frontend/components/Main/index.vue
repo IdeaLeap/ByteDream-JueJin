5db0c56058b679b58cc033920db858d671395160
Plumbiu <99574369+Plumbiu@users.noreply.github.com>
2023-01-22 11:17:09 +0800

    fix: 背景颜色

--- frontend/components/Main/index.vue
+++ frontend/components/Main/index.vue
@@ -7,7 +7,7 @@
     <div class="mt-5.17rem ">
       <Nav class="left-0" />
       <div class="timeline-container mt-0 relative 0.33rem">
-        <Post class="mr-21.667rem border-r-2 w-700px bg-green" />
+        <Post class="mr-21.667rem border-r-2 w-700px bg-white" />
         <Aside class="absolute top-0 right-0" />
       </div>
     </div>
