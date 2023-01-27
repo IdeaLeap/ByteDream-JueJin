4d33bde210c3c2b1fd323d70517fa8875affbaff
camera-2018 <2907618001@qq.com>
2023-01-27 17:47:26 +0800

    fix: 修复时间的跳变

--- frontend/composables/useTime.ts
+++ frontend/composables/useTime.ts
@@ -1,8 +1,8 @@
-function getChineseDate() {
+export function getChineseDate() {
   return new Date(
     new Date().getTime()
-      + new Date().getTimezoneOffset() * 60 * 1000
-      + 8 * 60 * 60 * 1000,
+    + new Date().getTimezoneOffset() * 60 * 1000
+    + 8 * 60 * 60 * 1000,
   )
 }
 
@@ -13,4 +13,19 @@ export function useTime(num: number) {
   const month = date.getMonth() + 1
   const day = date.getDate()
   return `${year}-${month}-${day}`
+}
+
+export function useDayParts() {
+  const state = ref('')
+  const hours = useDateFormat(useNow(), 'HH', { locales: 'zh-Hans-CN' })
+  const hour = parseInt(hours.value)
+  if (hour >= 0 && hour <= 11)
+    state.value = '早上好!'
+  else if (hour > 11 && hour <= 14)
+    state.value = '中午好!'
+  else if (hour > 14 && hour <= 18)
+    state.value = '下午好!'
+  else if (hour > 18 && hour <= 24)
+    state.value = '晚上好!'
+  return state
 }
