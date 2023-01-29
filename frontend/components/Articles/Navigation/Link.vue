b4dd4aafdd51291ca03d56e50e9da359cc4b08c2
Plumbiu <99574369+Plumbiu@users.noreply.github.com>
2023-01-29 10:51:43 +0800

    fix: 主题化颜色重复问题

--- frontend/components/Articles/Navigation/Link.vue
+++ frontend/components/Articles/Navigation/Link.vue
@@ -7,24 +7,24 @@ const artlistPath = useArtlistPath()
     <NuxtLink
       :to="`${artlistPath === '' ? '/' : artlistPath}`"
       :class="`${
-        $route.query.sort ? 'text-gray-500' : 'text-jj_link'
-      } border-r-1 focus:text-jj_link hover:text-jj_link`"
+        $route.query.sort ? 'text-gray-500' : 'text-link'
+      } border-r-1 focus:text-link hover:text-link`"
     >
       推荐
     </NuxtLink>
     <NuxtLink
       :to="`${artlistPath}?sort=newest`"
       :class="`${
-        $route.query.sort === 'newest' ? 'text-jj_link' : 'text-gray-500'
-      } border-r-1 focus:text-jj_link hover:text-jj_link`"
+        $route.query.sort === 'newest' ? 'text-link' : 'text-gray-500'
+      } border-r-1 focus:text-link hover:text-link`"
     >
       最新
     </NuxtLink>
     <NuxtLink
       :to="`${artlistPath}?sort=three_days_hottest`"
       :class="`${
-        ($route.query.sort && $route.query.sort?.indexOf('hottest') !== -1) ? 'text-jj_link' : 'text-gray-500'
-      } focus:text-jj_link hover:text-jj_link`"
+        ($route.query.sort && $route.query.sort?.indexOf('hottest') !== -1) ? 'text-link' : 'text-gray-500'
+      } focus:text-link hover:text-link`"
     >
       热榜
     </NuxtLink>
