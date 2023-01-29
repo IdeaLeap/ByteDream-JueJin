22f5d0f3080759cd60c5f9894e84cd348bec401d
camera-2018 <2907618001@qq.com>
2023-01-29 22:28:39 +0800

    fix: 广告关闭

--- frontend/components/Aside/Advertisements.vue
+++ frontend/components/Aside/Advertisements.vue
@@ -1,23 +1,25 @@
 <script setup lang="ts">
-defineProps({
+const props = defineProps({
   ads: {
     type: Object,
     required: true,
   },
 })
-const is_show = ref(true)
-const turn_off = () => {
-  is_show.value = false
+const is_show_list = reactive({ ...Array<Boolean> })
+for (let i = 0; i < props.ads.length; i++)
+  is_show_list[i] = true
+const turn_off = (i: number) => {
+  is_show_list[i] = false
 }
 </script>
 
 <template>
-  <div v-if="is_show">
-    <div v-for="i in ads" :key="i.img">
-      <div>
+  <div>
+    <div v-for="(i, index) in ads" :key="i.img">
+      <div v-if="is_show_list[index]">
         <div class="flex flex-row justify-end gap-2 group">
           <nuxt-img class="mb-5 rounded-sm shadow-[0_2px_2px_0_rgba(0,0,0,0.05)] " :src="i.img" />
-          <div i-carbon-close class="my-2 mx-1 absolute text-[#909090] opacity-50 text-[1.5rem] font-[1500] opacity-0 group-hover:opacity-100" @click="turn_off" />
+          <div i-carbon-close class="my-2 mx-1 absolute text-[#909090] opacity-50 text-[1.5rem] font-[1500] opacity-0 group-hover:opacity-100" @click="turn_off(parseInt(index))" />
         </div>
         <div class="font-[20px]">
           <div class="absolute translate-y-[-3.5rem] translate-x-[16.5rem] text-light-50 px-1.5 bg-[rgba(0,0,0,.2)] rounded-md border-white border-1">
