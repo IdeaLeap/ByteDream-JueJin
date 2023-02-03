ae0ba58f1f70fd6b861de05539a4193980bfff54
少轻狂 <1677568218@qq.com>
2023-02-04 00:36:11 +0800

    feat: 分类栏重构

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="">
-    <Navs class="relative h-5rem" />
+    <Navs class="relative h-[5rem] lt-sm:(h-[4.333rem])" />
     <slot class="relative mx-0 my-a w-100% max-w-960" />
   </main>
 </template>
