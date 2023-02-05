f8025d0a9194fb9269efcfc3fb00c6ae53101f58
Plumbiu <99574369+Plumbiu@users.noreply.github.com>
2023-02-05 18:39:58 +0800

    fix: 作者等级图片

--- frontend/components/ArticlesList/Item/AuthorInfo/index.vue
+++ frontend/components/ArticlesList/Item/AuthorInfo/index.vue
@@ -17,7 +17,7 @@ defineProps<{
       <div class="info">
         <div class="name">
           <span>{{ name }}</span>
-          <img :src="`/lv-${rank}.png`" alt="等级">
+          <img :src="`https://pan.marlene.top/d/share/jj/${rank}.png`" alt="等级">
         </div>
         <div class="text-[#8f969c]">
           {{ motto }}
