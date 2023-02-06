b8035c0b607494d188cc13d1881d3988da9695cd
Plumbiu <99574369+Plumbiu@users.noreply.github.com>
2023-02-06 15:55:30 +0800

    fix: 路由问题

--- frontend/components/ArticlesList/index.vue
+++ frontend/components/ArticlesList/index.vue
@@ -2,17 +2,22 @@
 const artlist = useArtlist(await useFetchPostData())
 const articleAds = (await useFetch('/api/global')).data.value.articleAds
 const isLoading = useState('isLoading', () => true)
-
+const route = useRoute()
 let pagenum = 1
 const addArtListItem = useThrottle(async () => {
   useScrollBottom() && artlist.value.push(...(await useFetchPostData(route.path, route.query?.sort, ++pagenum)))
 })
 provide('artlist', artlist)
 provide('ads', articleAds)
-watch(route, async () => {
+watch(route, async (r) => {
+  const paths = route.path.split('/')
+  const queryParams = {
+    type: paths[1],
+    tag: paths[2],
+  }
   artlist.value = []
-  artlist.value = await useFetchPostData(route.path, route.query?.sort, pagenum = 1)
-}, { deep: true })
+  artlist.value = await useFetchPostData(queryParams, route.query?.sort, pagenum = 1)
+}, { deep: true, immediate: true })
 onMounted(() => {
   (window as any).addEventListener('scroll', addArtListItem)
   isLoading.value = false
