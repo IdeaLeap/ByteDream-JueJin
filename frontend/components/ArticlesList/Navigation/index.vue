a91a1cb36e88eba0936541a73e742f4d3dea4aaf
Plumbiu <99574369+Plumbiu@users.noreply.github.com>
2023-02-16 16:03:30 +0800

    feat: 热度选择框懒加载

--- frontend/components/ArticlesList/Navigation/index.vue
+++ frontend/components/ArticlesList/Navigation/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="navigator">
     <ArticlesListNavigationLink />
-    <ArticlesListNavigationSelect />
+    <LazyArticlesListNavigationSelect v-if="$route.query?.sort?.indexOf('hottest')" />
   </div>
 </template>
 
