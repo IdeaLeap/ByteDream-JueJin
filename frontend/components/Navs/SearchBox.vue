222d7c6ba45eee8e62ca288437807135d926ac0b
lightstarts <1286518974@qq.com>
2023-02-17 18:56:19 +0800

    feat: 点击时会变宽

--- frontend/components/Navs/SearchBox.vue
+++ frontend/components/Navs/SearchBox.vue
@@ -145,4 +145,35 @@ button, input, select, textarea {
 img {
     border-style: none;
 }
+/*active*/
+/* @media (max-width: 1149px){
+.search-add :focus{
+    margin-right: .292rem;
+}}
+.search-add :focus{
+    flex: 1 1 auto;
+    height: 5rem;
+    display: flex;
+    justify-content: flex-end;
+}
+@media screen and (max-width: 1069px){
+.search-add-ul[data-v-96dfbeac] {
+    width: 32.667rem;
+}}
+
+@media screen and (max-width: 1350px){
+.search-add-ul[data-v-96dfbeac] {
+    width: 27.083rem;
+}}
+.search-add-ul[data-v-96dfbeac] {
+    display: flex;
+    justify-content: flex-start;
+}
+ul :focus{
+    padding: 0;
+    margin: 0;
+} */
+.bar7 input:focus {
+            width: 300px;
+        }
 </style>
